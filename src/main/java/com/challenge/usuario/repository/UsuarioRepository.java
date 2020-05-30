6fd4b747373c128435333b6c66df2d8d41ab5d99
Montival Junior <montival_junior@yahoo.com.br>
2020-05-30 13:19:44 -0300

    Implementado UserDetailsService e UsuarioRepository

--- src/main/java/com/challenge/usuario/repository/UsuarioRepository.java
+++ src/main/java/com/challenge/usuario/repository/UsuarioRepository.java
@@ -1,4 +1,10 @@
 package com.challenge.usuario.repository;
 
-public class UsuarioRepository {
+import com.challenge.usuario.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    UserDetails findByEmail(String email);
 }
--- src/main/java/com/challenge/usuario/service/UserDetailsImpl.java
+++ src/main/java/com/challenge/usuario/service/UserDetailsImpl.java
@@ -1,12 +1,20 @@
 package com.challenge.usuario.service;
 
+import com.challenge.usuario.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class UserDetailsImpl implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return this.usuarioRepository.findByEmail(email);
     }
+
+
 }
