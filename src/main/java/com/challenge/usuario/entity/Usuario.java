d91d8dbb52497140265b795e15b8867f342b2f45
Montival Junior <montival_junior@yahoo.com.br>
2020-05-30 13:13:04 -0300

    Adicionado pacote de service e Repository

--- src/main/java/com/challenge/usuario/entity/Usuario.java
+++ src/main/java/com/challenge/usuario/entity/Usuario.java
@@ -1,4 +1,4 @@
-package com.challenge.Usuario.entity;
+package com.challenge.usuario.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
---
+++ src/main/java/com/challenge/usuario/repository/UsuarioRepository.java
@@ -0,0 +1,4 @@
+package com.challenge.usuario.repository;
+
+public class UsuarioRepository {
+}
